dfe8f8f9eda677fba06aa08c928af1dc07a1874e
Riaan Taveras <riaantaveras@gmail.com>
2019-12-08 19:53:20 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,11 @@ end
 def print_first_directors_movie_titles
   directors_database
   puts directors_database[0][:movies]
-  
+  i = 0 
+  while i < directors_database[0][:movies].length 
+    puts directors_database[0][:movies][i][:title]
+    i += 1 
+  end
 end
 
  
